a82057695d66a584b8a4e302730d53c59d2ad665
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-23 12:58:07 +0600

    volunteer request and volunteers number decrese

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ggzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,27 +25,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-    const volunteerPostCollection = client.db("volunteerDB").collection("volunteerPosts");
+    const volunteerPostCollection = client
+      .db("volunteerDB")
+      .collection("volunteerPosts");
+    const volunteerRequestsCollection = client
+      .db("volunteerDB")
+      .collection("volunteerRequests");
 
     app.get("/all-posts", async (req, res) => {
-      const search = req.query.search
+      const search = req.query.search;
       let query = {
         title: {
           $regex: search,
-          $options: 'i',
-        }
-      }
+          $options: "i",
+        },
+      };
       const result = await volunteerPostCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.get('/post/:id', async(req, res)=>{
+    app.get("/post/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
-      const result = await volunteerPostCollection.findOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerPostCollection.findOne(query);
       res.send(result);
-    })
+    });
     app.post("/add-post", async (req, res) => {
       const newPost = req.body;
       const result = await volunteerPostCollection.insertOne(newPost);
@@ -54,15 +57,62 @@ async function run() {
     });
 
     // volunteer needs now
-    app.get('/volunteer-needs-now', async(req, res)=>{
-      const result = await volunteerPostCollection.find().sort({deadline: 1}).limit(6).toArray();
-      res.send(result)
-    })
+    app.get("/volunteer-needs-now", async (req, res) => {
+      const result = await volunteerPostCollection
+        .find()
+        .sort({ deadline: 1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
+    app.post("/volunteer-request", async (req, res) => {
+      const volunteerInfo = req.body;
+      const { email, postId } = volunteerInfo;
+
+      try {
+        const query = { email, postId };
+        const alreadyExist = await volunteerRequestsCollection.findOne(query);
+        console.log("If already exist-->", alreadyExist);
+
+        if (alreadyExist) {
+          return res
+            .status(400)
+            .send("You have already requested to volunteer for this post!");
+        }
+
+        const result = await volunteerRequestsCollection.insertOne(
+          volunteerInfo
+        );
+
+        const filter = { _id: new ObjectId(postId) };
+        const update = { $inc: { volunteersNumber: -1 } };
+        const updatedVolunteerNumberPost = await volunteerPostCollection.updateOne(
+          filter,
+          update
+        );
+        const updatedVolunteerNumberRequest = await volunteerRequestsCollection.updateOne(
+          {postId},
+          update
+        );
+
+
+        if (updatedVolunteerNumberPost.modifiedCount === 0) {
+          return res.status(500).send("Failed to update volunteer count.");
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ error: "Internal server error" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,12 +120,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
