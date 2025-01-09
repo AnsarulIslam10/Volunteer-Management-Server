66b392a18664626f5482784fb51d0b6af2d78c4c
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-09 22:48:00 +0600

    limit increse for volunteer needs now posts

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ async function run() {
       const result = await volunteerPostCollection
         .find()
         .sort({ deadline: 1 })
-        .limit(6)
+        .limit(8)
         .toArray();
       res.send(result);
     });
