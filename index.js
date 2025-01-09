790bd4c8736f4d1c4fac66139480825cc84616ed
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-10 01:07:03 +0600

    cors origin update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "https://volunteer-management-7efa7.web.app",
   ],
   credentials: true,
